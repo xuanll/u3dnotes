7113bd9ef72cb14dd535e7204d4a86848afd6c66
xuanll <xuanll@yeah.net>
2015-11-25 13:54:51 +0800

    add iOS64 require csharp file

---
+++ iOS64Require/AoTtype.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.ComponentModel;
+public class AoTtype : MonoBehaviour {
+	private static System.ComponentModel.Int64Converter _unused = new System.ComponentModel.Int64Converter();
+	private static System.ComponentModel.DecimalConverter _unused2 = new System.ComponentModel.DecimalConverter();
+	private static System.ComponentModel.ByteConverter _unused3 = new System.ComponentModel.ByteConverter();
+	private static System.ComponentModel.CollectionConverter _unused4 = new System.ComponentModel.CollectionConverter();
+	private static System.ComponentModel.CharConverter _unused5 = new System.ComponentModel.CharConverter();
+	private static System.ComponentModel.SByteConverter _unused6 = new System.ComponentModel.SByteConverter();
+	private static System.ComponentModel.Int16Converter _unused7 = new System.ComponentModel.Int16Converter();
+	private static System.ComponentModel.UInt16Converter _unused8 = new System.ComponentModel.UInt16Converter();
+	private static System.ComponentModel.Int32Converter _unused9 = new System.ComponentModel.Int32Converter();
+	private static System.ComponentModel.UInt32Converter _unused10 = new System.ComponentModel.UInt32Converter();
+	private static System.ComponentModel.Int64Converter _unused11 = new System.ComponentModel.Int64Converter();
+	private static System.ComponentModel.UInt64Converter _unused12 = new System.ComponentModel.UInt64Converter();
+	private static System.ComponentModel.DoubleConverter _unused13 = new System.ComponentModel.DoubleConverter();
+	private static System.ComponentModel.SingleConverter _unused14 = new System.ComponentModel.SingleConverter();
+	private static System.ComponentModel.BooleanConverter _unused15 = new System.ComponentModel.BooleanConverter();
+	private static System.ComponentModel.StringConverter _unused16 = new System.ComponentModel.StringConverter();
+	private static System.ComponentModel.DateTimeConverter _unused17 = new System.ComponentModel.DateTimeConverter();
+	//private static System.ComponentModel.EnumConverter _unused18 = new System.ComponentModel.EnumConverter(typeof(<any your enum>));
+	private static System.ComponentModel.TimeSpanConverter _unused19 = new System.ComponentModel.TimeSpanConverter();
+}
